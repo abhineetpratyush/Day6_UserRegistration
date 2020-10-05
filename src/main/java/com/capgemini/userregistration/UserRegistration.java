12c9a582f6fb834c30ece9aef131bfc83e2e86e0
abhineetpratyush <abhineetpratyush@gmail.com>
2020-10-05 15:00:26 +0530

    [abhineet] refactor : refactor to add ability to validate last name

--- src/main/java/com/capgemini/userregistration/UserRegistration.java
+++ src/main/java/com/capgemini/userregistration/UserRegistration.java
@@ -5,20 +5,33 @@ import org.apache.logging.log4j.Logger;
 import java.util.Scanner;
 
 public class UserRegistration {
-	
+
 	private static final Logger LOG = LogManager.getLogger(UserRegistration.class);
- 	
+
 	private boolean validateFirstName(String firstName) {
 		return firstName.matches("^[A-Z]{1}[a-z]{2,}$");
 	}
+
+	private boolean validateLastName(String lastName) {
+		return lastName.matches("^[A-Z]{1}[a-z]{2,}$");
+	}	
+
 	public static void main( String[] args ) {
+		UserRegistration userRegistration = new UserRegistration();
 		Scanner sc = new Scanner(System.in);
-    	LOG.info("First Name: ");
-    	String firstName = sc.nextLine();
-    	UserRegistration userRegistration = new UserRegistration();
-    	if(userRegistration.validateFirstName(firstName))
-    		LOG.info("Valid First Name");
-    	else
-    		LOG.info("Invalid First Name");
-    } 
+		LOG.info("First Name: ");
+		String firstName = sc.nextLine();
+		if(userRegistration.validateFirstName(firstName))
+			LOG.info("Valid First Name");
+		else
+			LOG.info("Invalid First Name");
+		
+		LOG.info("Last Name: ");
+		String lastName = sc.nextLine();
+		if(userRegistration.validateLastName(lastName))
+			LOG.info("Valid Last Name");
+		else
+			LOG.info("Invalid Last Name");
+		
+	} 
 }
