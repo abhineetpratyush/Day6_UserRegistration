c486bb95feb52012919ae04c6e1d422152d8e663
abhineetpratyush <abhineetpratyush@gmail.com>
2020-10-10 20:34:33 +0530

    [abhineet] refactor : refactor the code to use lambda expressions to validate user entries

--- src/main/java/com/capgemini/userregistration/UserRegistration.java
+++ src/main/java/com/capgemini/userregistration/UserRegistration.java
@@ -4,40 +4,51 @@ import java.util.Scanner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@FunctionalInterface
+interface UserValidationInterface{
+	boolean validate(String userInput);
+}
+
 public class UserRegistration {
 
 	private static final Logger LOG = LogManager.getLogger(UserRegistration.class);
+	UserValidationInterface validateFirstName = firstName -> firstName.matches("^[A-Z]{1}[a-z]{2,}$");
+	UserValidationInterface validateLastName = lastName -> lastName.matches("^[A-Z]{1}[a-z]{2,}$");
+	UserValidationInterface validateEmailId = emailId -> emailId.matches("^[a-zA-Z0-9_]+([.+-]{1}[a-zA-Z0-9_]+)*[@]{1}[a-zA-Z0-9]+[.]{1}[a-zA-Z0-9]{2,}([.]{1}[a-zA-Z]{2,})?$");
+	UserValidationInterface validateMobileNo = mobileNo -> mobileNo.matches("^[0-9]{2}[ ][0-9]{10}$");
+	UserValidationInterface validatePassword = password -> password.matches("(?=^.{8,}$)(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]*[@#$%_][0-9a-zA-Z]*");
 
 	public boolean validateFirstName(String firstName) throws UserRegistrationException {
-		if(firstName.matches("^[A-Z]{1}[a-z]{2,}$"))
+		
+		if(validateFirstName.validate(firstName))
 			return true;
 		else
 			throw new UserRegistrationException("Invalid First Name");
 	}
 
 	public boolean validateLastName(String lastName) throws UserRegistrationException {
-		if(lastName.matches("^[A-Z]{1}[a-z]{2,}$"))
+		if(validateLastName.validate(lastName))
 			return true;
 		else 
 			throw new UserRegistrationException("Invalid Last Name");
 	}	
 
 	public boolean validateEmail(String emailId) throws UserRegistrationException {
-		if(emailId.matches("^[a-zA-Z0-9_]+([.+-]{1}[a-zA-Z0-9_]+)*[@]{1}[a-zA-Z0-9]+[.]{1}[a-zA-Z0-9]{2,}([.]{1}[a-zA-Z]{2,})?$"))
+		if(validateEmailId.validate(emailId))
 			return true;
 		else
 			throw new UserRegistrationException("Invalid Email Id");
 	}
 
 	public boolean validateMobileNo(String mobileNo) throws UserRegistrationException {
-		if(mobileNo.matches("^[0-9]{2}[ ][0-9]{10}$"))
+		if(validateMobileNo.validate(mobileNo))
 			return true;
 		else
 			throw new UserRegistrationException("Invalid Mobile No");
 	}
 
 	public boolean validatePassword(String password) throws UserRegistrationException {
-		if(password.matches("(?=^.{8,}$)(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]*[@#$%_][0-9a-zA-Z]*"))
+		if(validatePassword.validate(password))
 			return true;
 		else
 			throw new UserRegistrationException("Invalid Password");
